4998915f89b1270b801a04ac67f016f6f844572e
USER1 <emoleg2008@gmail.com>
2024-05-14 22:41:11 +0500

    My Project

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,33 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
-
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
-# Create your views here.
-
 def post_share(request, post_id):
-    post = get_object_or_404(Post , pk=post_id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, pk=post_id, status=Post.Status.PUBLISHED)
+
+    sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, cd['email'],
+                      [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
 
 def post_list(request):
     posts = Post.published.all()
@@ -31,18 +41,23 @@ def post_list(request):
     except PageNotAnInteger:
         post_list = paginator.page(1)
 
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': post_list})
+    return render(request, 'blog/post/list.html', {'posts': post_list})
 
 
 def post_detail(request, post, year, month, day):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post})
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post,'comments': comments, 'form': form})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
